e385aaa474b26641feb94cc49aa4c37b8508647b
Ryan Gomoto <ryan@cyrusbio.com>
2018-06-24 15:30:31 -0700

    Call and answer

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,49 @@ interface AppState {}
 class App extends React.Component<AppProps, AppState> {
   private _localVideo: React.RefObject<HTMLVideoElement>;
   private _remoteVideo: React.RefObject<HTMLVideoElement>;
+  private _localStream: MediaStream | null;
   private _peer: Peer;
 
   constructor(props: AppProps) {
     super(props);
     this._localVideo = React.createRef();
     this._remoteVideo = React.createRef();
+    this._localStream = null;
     this._peer = new Peer({}); // types?
   }
 
   async componentDidMount() {
     const localVideo = this._localVideo.current as HTMLVideoElement;
-    const localStream = await navigator.mediaDevices.getUserMedia({video: true});
-    localVideo.srcObject = localStream;
+    this._localStream = await navigator.mediaDevices.getUserMedia({video: true});
+    localVideo.srcObject = this._localStream;
 
     // when remote calls
     this._peer.on('call', (call: Peer.MediaConnection) => {
-      // answer call with local video stream
-      call.answer(localStream);
+      if (!window.confirm('Answer call?')) {
+        return;
+      }
+      this.answer(call);
+    });
+  }
 
-      // capture remote video stream
-      const remoteVideo = this._localVideo.current as HTMLVideoElement;
-      call.on('stream', (remoteStream) => {
-        remoteVideo.srcObject = remoteStream;
-      });
+  call(): void {
+    console.log('call()');
+    const call = this._peer.call('cheesemonkeybananabread', this._localStream);
+    this.captureCallStream(call);
+  }
+
+  answer(call: Peer.MediaConnection): void {
+    console.log('answer()');
+    // answer call with local video stream
+    call.answer(this._localStream);
+    this.captureCallStream(call);
+  }
+
+  captureCallStream(call: Peer.MediaConnection): void {
+    // capture remote video stream
+    const remoteVideo = this._remoteVideo.current as HTMLVideoElement;
+    call.on('stream', (remoteStream) => {
+      remoteVideo.srcObject = remoteStream;
     });
   }
 
@@ -46,6 +65,9 @@ class App extends React.Component<AppProps, AppState> {
           autoPlay
           ref={this._remoteVideo}
         ></video>
+        <div className="controls">
+          <button onClick={this.call.bind(this)}>Call</button>
+        </div>
       </div>
     );
   }
