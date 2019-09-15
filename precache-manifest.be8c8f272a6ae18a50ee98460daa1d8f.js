a7338261b2e51c13e28b34e0cb6d34d40f07e37d
Ryan Gomoto <ryan@cyrusbio.com>
2019-09-14 22:38:08 -0700

    Updates

---
+++ precache-manifest.be8c8f272a6ae18a50ee98460daa1d8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2583c5f794ee8d1a491a732905c05b03",
+    "url": "/air/index.html"
+  },
+  {
+    "revision": "8ffd8e72d0b119dba033",
+    "url": "/air/static/css/main.341761e4.chunk.css"
+  },
+  {
+    "revision": "7f19cdb22674c2bf7d35",
+    "url": "/air/static/js/2.71267446.chunk.js"
+  },
+  {
+    "revision": "8ffd8e72d0b119dba033",
+    "url": "/air/static/js/main.689df99b.chunk.js"
+  },
+  {
+    "revision": "1b06ee4c077a409985da",
+    "url": "/air/static/js/runtime~main.da2c925c.js"
+  }
+]);
